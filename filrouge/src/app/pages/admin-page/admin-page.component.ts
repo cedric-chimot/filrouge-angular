fa393a0683617884bbae9242f78e94441b7179f1
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2024-02-06 17:46:37 +0100

    récupération des stagiares et affichage dashboard

--- filrouge/src/app/pages/admin-page/admin-page.component.ts
+++ filrouge/src/app/pages/admin-page/admin-page.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { StagiaireServiceService } from '../../services/stagiaires/stagiaire-service.service';
 
 
 @Component({
@@ -15,5 +16,37 @@ import { RouterModule } from '@angular/router';
     imports: [AdminNavbarComponent, AdminSidebarComponent, MatToolbarModule, MatIconModule, MatButtonModule, RouterModule]
 })
 export class AdminPageComponent {
+    nbStagiaires: number = 0; 
+    nbCandidats: number = 0;
+
+    constructor(private stagiaireService: StagiaireServiceService) {}
+  
+    ngOnInit(): void {
+      this.stagiaireService.getNbStagiaires()
+        .subscribe({
+            next: (nbStagiaires: number) => {
+                this.nbStagiaires = nbStagiaires;
+            },
+            error: (error) => {
+                console.error('Erreur lors de la récupération du nombre de stagiaires', error);
+            },
+            complete: () => {
+                console.log("Récupération complète");
+            }
+        });
+
+        this.stagiaireService.getNbCandidats()
+        .subscribe({
+            next: (nbCandidats: number) => {
+                this.nbCandidats = nbCandidats;
+            },
+            error: (error) => {
+                console.error('Erreur lors de la récupération du nombre de stagiaires', error);
+            },
+            complete: () => {
+                console.log("Récupération complète");
+            }
+        });
+    }
 
 }
--- filrouge/src/app/services/stagiaires/stagiaire-service.service.ts
+++ filrouge/src/app/services/stagiaires/stagiaire-service.service.ts
@@ -11,7 +11,20 @@ export class StagiaireServiceService {
 
   constructor(private httpClient: HttpClient) {}
 
+  getStagiaires(): Observable<Stagiaires[]>{
+    return this.httpClient.get<Stagiaires[]>(`${this.apiUrl}/stagiaires/all`);
+  }
+
+  getNbStagiaires(): Observable<number> {
+    return this.httpClient.get<number>(`${this.apiUrl}/stagiaires/nbStagiaires`);
+  }
+  
+  getNbCandidats(): Observable<number> {
+    return this.httpClient.get<number>(`${this.apiUrl}/stagiaires/nbCandidats`);
+  }
+  
   createStagiaire(stagiaire: Stagiaires): Observable<Stagiaires>{
     return this.httpClient.post<Stagiaires>(`${this.apiUrl}/stagiaires/create`, stagiaire);
   }
+
 }
